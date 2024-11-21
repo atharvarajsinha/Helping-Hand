adb12ebd2aaf635afd3ef75d21ec23c773ce9375
atharvarajsinha <sinhaatharva@gmail.com>
2024-11-21 09:20:37 +0530

    Modified

--- data.js
+++ data.js
@@ -1,11 +1,12 @@
 const fundraisers = [
-    {
+  {
       image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSngsGpZFUEnTqL11VPRw-eO1Vq8UI2nE1ScAwkPURVutBHJ1d100bI8b4tfHlGdmIo3Sk&usqp=CAU",
       title: "Medical Emergency for Ramesh",
       person: "Ramesh Kumar",
       category : "Medical",
       amountToBeRaised: "Rs. 1,00,000",
       amountRaised: "Rs. 75,000",
+      descrition: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque vero facere nam iusto eligendi, soluta sit commodi repudiandae in est pariatur aspernatur dolores. Pariatur assumenda voluptatum autem iste fugiat id saepe ut.',
     },
     {
       image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzq0rvB3FVy7GA09isjqckUTneW1BWUGnNbT65SE57hBfcxLju7vhJOhkETeAe_wDXQ4M&usqp=CAU",
@@ -14,6 +15,7 @@ const fundraisers = [
       category : "Education",
       amountToBeRaised: "Rs. 50,000",
       amountRaised: "Rs. 23,300",
+      descrition: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque vero facere nam iusto eligendi, soluta sit commodi repudiandae in est pariatur aspernatur dolores. Pariatur assumenda voluptatum autem iste fugiat id saepe ut.',
     },
     {
       image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjl4SEZfjHtGJvK93V6AVzmjPL99K6jP1x6Q&s",
@@ -22,5 +24,6 @@ const fundraisers = [
       category : "Animals",
       amountToBeRaised: "Rs. 15,000",
       amountRaised: "Rs. 9,000",
+      descrition: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque vero facere nam iusto eligendi, soluta sit commodi repudiandae in est pariatur aspernatur dolores. Pariatur assumenda voluptatum autem iste fugiat id saepe ut.',
     },
 ];
--- script.js
+++ script.js
@@ -91,16 +91,3 @@ function createCards(data) {
 }
 
 createCards(fundraisers);
-
-function donate() {
-    const element = document.getElementById("donation-input");
-    const name = document.getElementById("donation-name");
-    const amt = parseFloat(element.value);
-    if (amt >= 50) {
-        alert(`Thank You ${name.value} for donating ₹${amt}!`);
-        name.value = "";
-        element.value = "";
-    } else {
-        alert("Please enter an amount of ₹50 or more.");
-    }
-}
